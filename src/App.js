df6c8f197c7a0c9485cb9e9564a03c83b5446373
Anes <y0un35@ya.ru>
2022-07-18 18:37:22 +0100

    test_HourglassSpinner

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import React, { Suspense } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import Type from './component/TypesPr/Type'
-import CircleSpinner from './component/Element/Spinner'
+import {HourglassSpinner} from './component/Element/Spinner'
 const Home = React.lazy(() => import('./component/home'))
+// const Type = React.lazy(()=> import('./component/TypesPr/Type'))
+
 // import 'https://scripts.sirv.com/sirv.js'
 
 
 
 class App extends React.Component {
   render() {
-    return (<Suspense  fallback={<CircleSpinner  />}>
+    return (<Suspense  fallback={<HourglassSpinner  />}>
       <Routes>
         <Route path="/" exact element={<Home navTheme='auto' />} />
-        <Route path="/Spinner"  exact element={<CircleSpinner />} />
-        <Route path="/Type"  exact element={<Type />} />
+        <Route path="/Spinner"  exact element={<HourglassSpinner />} />
+        {/* <Route path="/Type"  exact element={<Type />} /> */}
       </Routes>
     </Suspense>
     );
